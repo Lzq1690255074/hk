4258a80356a82182cbbd659bf4c6464f07e5c56d
Administrator <Administrator@OS-201907151801>
2019-08-12 17:16:54 +0800

    1

---
+++ src/com/icss/Demo1.java
@@ -0,0 +1,15 @@
+package com.icss;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.println(j + "*" + "=" + j * i + "\t");
+
+			}
+			System.out.println();
+		}
+	}}
+
+          
